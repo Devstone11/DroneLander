c59a8599640392a58c7266c8573c8fb118094c8d
Devin Stone <SESA447754@schneider-electric.com>
2017-10-10 14:34:18 -0600

    Added Mobile Center crash and analytics support.

--- DroneLander/DroneLander/Common/CoreConstants.cs
+++ DroneLander/DroneLander/Common/CoreConstants.cs
@@ -17,4 +17,9 @@ namespace DroneLander.Common
         public const double StartingFuel = 1000.0;
         public const double StartingThrust = 0.0;
     }
+    public static class MobileCenterConstants
+    {
+        public const string AndroidAppId = "28925959-7be2-41e6-a332-150647d43e47";
+        public const string iOSAppId = "";
+    }
 }
